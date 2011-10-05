0cb3744ffbf09f26fb408c3f2c49a3a75e23bff6
Kristian Meier <m.kristian@web.de>
2011-10-05 20:35:59 +0530

    fix errors on rake tasks like db:sessions:create

--- lib/generators/rails/active_record/active_record_generator.rb
+++ lib/generators/rails/active_record/active_record_generator.rb
@@ -1,40 +1,46 @@
-require 'rails/generators/active_record'
+if defined?(ActiveRecord) && !defined?(Rake) #when run from rake ignore this as well since otherwise active_record is not found
+  require 'rails/generators/active_record/model/model_generator'
 
-module ActiveRecord
-  module Generators
-    class ModelGenerator < Base
-      include ::Ixtlan::Generators::Singleton
-
-      argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
-
-      check_class_collision
-
-      class_option :migration,  :type => :boolean
-      class_option :timestamps, :type => :boolean
-      class_option :parent,     :type => :string, :desc => "The parent class for the generated model"
-
-      def create_migration_file
-        return unless options[:migration] && options[:parent].nil?
-        migration_template "migration.rb", "db/migrate/create_#{table_name}.rb"
-      end
-
-      def create_model_file
-        template 'model.rb', File.join('app/models', class_path, "#{file_name}.rb")
-      end
-
-      def create_module_file
-        return if class_path.empty?
-        template 'module.rb', File.join('app/models', "#{class_path.join('/')}.rb") if behavior == :invoke
-      end
-
-      hook_for :test_framework
-
-      protected
-
+  module ActiveRecord
+    module Generators
+      class ModelGenerator < Base
+        include ::Ixtlan::Generators::Singleton
+        
+        argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+        
+        check_class_collision
+        
+        class_option :migration,  :type => :boolean
+        class_option :timestamps, :type => :boolean
+        class_option :modified_by, :type => :string
+        class_option :parent,     :type => :string, :desc => "The parent class for the generated model"
+        
+        def create_migration_file
+          return unless options[:migration] && options[:parent].nil?
+          migration_template "migration.rb", "db/migrate/create_#{table_name}.rb"
+        end
+        
+        def create_model_file
+          template 'model.rb', File.join('app/models', class_path, "#{file_name}.rb")
+        end
+        
+        def create_module_file
+          return if class_path.empty?
+          template 'module.rb', File.join('app/models', "#{class_path.join('/')}.rb") if behavior == :invoke
+        end
+        
+        hook_for :test_framework
+        
+        protected
+        
         def parent_class_name
           options[:parent] || "ActiveRecord::Base"
         end
-
+        
+      end
     end
   end
+  
+else
+  raise "ignore this file"
 end
